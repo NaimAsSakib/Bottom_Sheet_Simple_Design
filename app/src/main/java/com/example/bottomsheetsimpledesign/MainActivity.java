c1f06ad47eecfc8fca19505797e524c7288d4de8
NaimAsSakib <rifat.sakib90@gmail.com>
2022-01-02 10:38:16 +0600

    Changed Fragment with BottomSheetDialogFragment & code is working

--- app/src/main/java/com/example/bottomsheetsimpledesign/MainActivity.java
+++ app/src/main/java/com/example/bottomsheetsimpledesign/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.bottomsheetsimpledesign;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.btnShow);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            Bottom_Sheet_Fragment bottomSheetFragment=new Bottom_Sheet_Fragment();
+            bottomSheetFragment.show(getSupportFragmentManager(),bottomSheetFragment.getTag());
+            }
+        });
+
     }
 }
